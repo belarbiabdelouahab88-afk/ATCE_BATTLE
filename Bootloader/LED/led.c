3fc50cd6512372ac22925e19dcf73ab513823f35
Cheribin <steevejonathan13@gmail.com>
2026-03-24 12:52:02 +0100

    Update led.c

--- Bootloader/LED/led.c
+++ Bootloader/LED/led.c
@@ -1,18 +1,22 @@
 #include <avr/io.h> //E/S ex PORTB 
 #define F_CPU 16000000UL
+#define F_USB 16000000UL
 #include <util/delay.h>
+#include "VirtualSerial.h"
 
 int main(void){
-
+	SetupHardware();
 	MCUCR|=(1<<JTD);
 	MCUCR|=(1<<JTD);
 
-	DDRF |= (1 << PF1)|(1 << PF4)|(1 << PF5)|(1 << PF6);
-	PORTF |= (1 << PF1)|(1 << PF4)|(1 << PF5)|(1 << PF6);
+	DDRF |= (1 << PORTF1)|(1 << PORTF4)|(1 << PORTF5)|(1 << PORTF6);
+	PORTF |= (1 << PORTF1)|(1 << PORTF4)|(1 << PORTF5)|(1 << PORTF6);
 	
 	while (1){
-		PORTF^=1<<PORTF1;PORTF^=1<<PORTF4;PORTF^=1<<PORTF5;PORTF^=1<<PORTF6;
-		_delay_ms(500);
+		//PORTF^=1<<PORTF1;
+				PORTF^=1<<PORTF1;PORTF^=1<<PORTF4;PORTF^=1<<PORTF5;PORTF^=1<<PORTF6;
+
+		_delay_ms(100);
 	}
 	return 0;
 }
