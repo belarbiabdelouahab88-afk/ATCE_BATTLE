abae9b6d471baef3b1859e7d4fb9ec59a713ef71
Cheribin <steevejonathan13@gmail.com>
2026-03-24 08:49:57 +0100

    Update blink.c

--- Bootloader/blink.c
+++ Bootloader/blink.c
@@ -3,14 +3,12 @@
 #include <util/delay.h>
 
 int main(void){
-  DDRB |=1<<PORTB5;
-  DDRE |=1<<PORTE6;
-  PORTB |= 1<<PORTB5;
-  PORTE &= ~(1<<PORTE6);
+  DDRF |=1<<PORTF1;
+  PORTF |= 1<<PORTF1;
 
   while (1){
     /* Clignotement des LEDS */
-  PORTB^=1<<PORTB5;PORTE^=1<<PORTE6;
+  PORTF^=1<<PORTF1;
   _delay_ms(500);
   }
   return 0;
