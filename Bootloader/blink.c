3ab994e871c427ba93ad903ee4eb3b8f033fc179
stdunlogic <yasserkheddaoui13@gmail.com>
2026-03-24 13:01:36 +0100

    Update blink.c

--- Bootloader/blink.c
+++ Bootloader/blink.c
@@ -1,8 +1,11 @@
 #include <avr/io.h> //E/S ex PORTB 
 #define F_CPU 16000000UL
+#define F_USB 16000000UL
 #include <util/delay.h>
+#include "VirtualSerial.h"
 
 int main(void){
+  SetupHardware();
   DDRF |=1<<PORTF1;
   PORTF |= 1<<PORTF1;
 
