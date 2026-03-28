fd7e89c3b0863e665025f2271f437ac4e9ecf131
Cheribin <steevejonathan13@gmail.com>
2026-03-28 18:45:34 +0100

    Create InputCap.c
    Capture du signal 1PPS sur le compteur du Timer1

---
+++ INPUT capture/InputCap.c
@@ -0,0 +1,69 @@
+//Declenchement de l'interruption capture edge select pour detecter le signal 1PPS sur les fronts montants du Timer1
+#define F_CPU 16000000UL     
+#define F_USB 16000000UL     
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "VirtualSerial.h"
+#include <util/delay.h>
+
+extern USB_ClassInfo_CDC_Device_t VirtualSerial_CDC_Interface;
+extern FILE USBSerialStream;
+
+volatile signed int pps2 = 0;        // valeur actuelle du timer (ICR1)
+volatile signed int overflow = 0;     // nombre d'overflow entre deux PPS
+volatile signed int nOVF = 0;         // copie pour calcul
+volatile signed long f = 0;           // fréquence calculée (nombre de ticks)
+volatile signed int delta = 0;        // différence entre deux captures
+volatile signed char flag = 0;        // indique qu'une nouvelle mesure est prête
+
+ISR (TIMER1_OVF_vect) {
+    overflow++;
+}
+
+
+ISR (TIMER1_CAPT_vect) {
+
+    pps2 = ICR1;
+    nOVF = overflow;
+    f = (((signed long)nOVF)*65536)+pps2;
+    
+    TCNT1 =0;
+    overflow = 0;
+    flag = 1;
+}
+
+void input_capture () {
+
+    DDRD &= ~(1 << PD4);
+    // PD4 en entrée → reçoit le signal 1 PPS du GPS
+
+    TCCR1B |= (1 << ICES1);
+    // sélection du front montant pour la capture
+
+    TIMSK1 |= (1 << ICIE1) | (1 << TOIE1);
+    // active :
+    // - interruption capture (ICIE1)
+    // - interruption overflow (TOIE1)
+
+    TCCR1B |= (1 << CS10);
+    // prescaler = 1 
+}
+
+int main(void) {
+
+    SetupHardware();
+    CDC_Device_CreateStream(&VirtualSerial_CDC_Interface, &USBSerialStream);
+    input_capture();
+    GlobalInterruptEnable();
+    sei(); 
+
+    while (1) {
+        if (flag==1) {
+            fprintf(&USBSerialStream, "%u,%u,%ld\n\r", nOVF,pps2, f);                     
+            flag = 0;
+        }      
+        CDC_Device_ReceiveByte(&VirtualSerial_CDC_Interface);
+        CDC_Device_USBTask(&VirtualSerial_CDC_Interface);
+        USB_USBTask();
+    }
+}
