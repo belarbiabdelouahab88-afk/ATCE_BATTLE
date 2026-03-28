0a115cdd079d77976819e2454d233136217d641e
Cheribin <steevejonathan13@gmail.com>
2026-03-28 17:41:16 +0100

    Update fastpwm.c
    Generation d'une PWM_fast_mode sur le portD7 de l'atmega32u4

--- pmw/fastpwm.c
+++ pmw/fastpwm.c
@@ -1,3 +1,4 @@
+//Generation de la PWM du timer4 d'une resolution de 10 bits sur la sortie PD7 de l'atemega32u4
 #define F_CPU 16000000UL
 #define F_USB 16000000UL
 #include <avr/io.h>
@@ -8,23 +9,24 @@ extern USB_ClassInfo_CDC_Device_t VirtualSerial_CDC_Interface;
 extern FILE USBSerialStream;
 
 void init_PWM () {
-    
+	
     // PD7 = sortie OC4D
     DDRD |= (1 << PD7);
     
-    // Mode PWM : Phase & Frequency Correct (WGM40 = 1)
-    TCCR4D = (1 << WGM40);
+    // Activation du mode PWM_Fast
+	TCCR4D = (1 << PWM4X); 
 
-    // Activer OC4D + PWM4D
+	//Activation de la PWM
     TCCR4C = (1 << COM4D1) | (1 << PWM4D);
+    TCCR4C &= ~(1 << COM4D0);
 
-    // TOP = 1023 (10 bits)
-    TC4H =  0x03; // Masque sur les 2 bits de poids fort
-    OCR4C =  0xFF;
+    // Resolution de 1023
+    TC4H =  0x03;  // Masque sur les 2 bits de poids fort
+	OCR4C =  0xFF;
 
-    // Duty cycle = 50% → 512
+    // Rapport cyclique
     TC4H = 0x01; 
-    OCR4D =0xFF;
+	OCR4D =0xFF;
 
     // Prescaler = 1
     TCCR4B = (1 << CS40);
@@ -34,7 +36,8 @@ int main(void) {
     init_PWM();
     SetupHardware();
     CDC_Device_CreateStream(&VirtualSerial_CDC_Interface, &USBSerialStream);
-    GlobalInterruptEnable();
-    while (1) {    
+	GlobalInterruptEnable();
+    while (1) {	
+		
         }
 }
